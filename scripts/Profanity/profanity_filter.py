903f2befc15407459e992c3aed0b4981e4f16fd7
Himanshu Chhabra <himanshu.chhabra9304@gmail.com>
2019-04-21 01:21:57 -0400

    Updated profanity script and added new files

--- scripts/Profanity/profanity_filter.py
+++ scripts/Profanity/profanity_filter.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[ ]:
-
-
 # -------------------------- importing necessary modules --------------------------
 
 import csv                                    # writing results to .csv file
@@ -11,16 +6,14 @@ import re                                     # regular expression
 import pandas as pd                           # for data frames
 import matplotlib.pyplot as plt               # for plots
 import numpy as np                            # for maths
-plt.style.use('ggplot')                       # for plots 
-get_ipython().run_line_magic('matplotlib', 'inline')
-
+plt.style.use('ggplot')                       # for plots
 # set jupyter's max row display
 pd.set_option('display.max_row', 1000)
 # set jupyter's max column width to 50
 pd.set_option('display.max_columns', 50)
 csv_filepath = "../../csv_files/"                             # directory path to .csv files
 files = ["comments_tech.csv","comments_comedy.csv","comments_news.csv","comments_TV.csv"]   # list of files to be analyzed for profanity
-txt_filepath = "txt files/"
+txt_filepath = "text_files/"
 
 
 # In[ ]:
@@ -54,7 +47,7 @@ def find_profanity(filepath,filename,set_filter_stopwords,badwords):
     my_csv = pd.read_csv(filepath, na_filter=True, keep_default_na=False)   # importing comments from .csv file 
     column = my_csv.text                                                    # storing data only from 'text' column of the .csv file
     
-    with open('results_' + filename, 'a+', newline='') as outputfile:  # opening required files to read and write
+    with open('results_' + filename, 'w+', newline='') as outputfile:  # opening required files to read and write
         results_write = csv.writer(outputfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         results_write.writerows([["Comments","Profine_words", "No_words_per_comment","No_filtered_words", "Is_profine", "Avg_profine_per_comment", "Avg_profine_per_filter_comment", "No_profane_words"]])   # writing column headers
         count = 0
@@ -104,7 +97,7 @@ def plot():
     # --------------< reading result csv files and storing No_profane_words in data frame >--------------------
     csv_news = pd.read_csv('results_comments_news.csv')
     csv_tv = pd.read_csv('results_comments_TV.csv')
-    csv_comedy = pd.read_csv('results_comments_comedy_good.csv')
+    csv_comedy = pd.read_csv('results_comments_comedy.csv')
     csv_tech = pd.read_csv('results_comments_tech.csv')
     profane_news = csv_news.No_profane_words
     profane_tv = csv_tv.No_profane_words
@@ -130,14 +123,15 @@ def plot():
     ax = df.plot(kind='bar', figsize=(12,9), rot = 0, fontsize=13, x='Category',y='Percentages', legend = False);
     ax.set_alpha(0.75)
     ax.set_title("Percentage profanity per category\n", fontsize=18)
-    ax.set_xlabel("Percent profanity", fontsize=16);
-    ax.set_ylabel("Category", fontsize=16);
+    ax.set_xlabel("categories", fontsize=16);
+    ax.set_ylabel("% profanity", fontsize=16);
     ax.set_yticks([0,2,4,6,8,10,12])
     for p in ax.patches: 
             ax.annotate(np.round(p.get_height(),decimals=2), (p.get_x()+p.get_width()/2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points', fontsize=13)
+    
+    plt.savefig("profanity_analysis.png")
+    plt.show(block=True)
 
-
-# In[ ]:
 
 
 if __name__ == "__main__":
@@ -148,7 +142,7 @@ if __name__ == "__main__":
     for wrd in lst_stopwords:
         set_filter_stopwords.add(re.sub(r"\n", '', wrd, flags=re.MULTILINE))  # removing newline characters from each stopword
     stopwords_inp_file.close()
-    badwords = get_set(txt_filepath + 'badwords.txt')                         # retreiving set of bad words
+    badwords = get_set(txt_filepath + 'badwords.txt')                         # retrieving set of bad/profane words
     
     # --------< Calculating profanity for various categories >--------
     for filename in files:
